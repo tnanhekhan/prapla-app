c54a933207757bc85799a6cbdffe417d7b00e222
MarvinMichel <mmhsernee@gmail.com>
2021-05-25 15:39:09 +0200

    Update header styling

--- components/Logo.vue
+++ components/Logo.vue
@@ -1,18 +1,17 @@
 <template>
   <header>
-    <h1 class="NuxtLogo">Praatje-Plaatje</h1>
+    <h1>Praatje-Plaatje</h1>
   </header>
 </template>
 
-<style>
-.NuxtLogo {
-  animation: 1s appear;
-  margin: auto;
-}
-
-@keyframes appear {
-  0% {
-    opacity: 0;
+<style scoped>
+  header {
+    height: 12.5vh;
+    padding: 1rem;
   }
-}
+
+  h1{
+  	font-family: sans-serif;
+  	text-align: center;
+  }
 </style>
