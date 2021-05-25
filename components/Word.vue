b130b3bb1abfefa40307f3ac7586ef8fa4ce2830
MarvinMichel <mmhsernee@gmail.com>
2021-05-25 15:22:03 +0200

    Move prototype to Vue

---
+++ components/Word.vue
@@ -0,0 +1,23 @@
+<template>
+  <section>
+    <figure></figure>
+    <p class="word">{{ word }}</p>
+  </section>
+</template>
+
+<script>
+export default {
+  props: {
+    word: {
+      type: String
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+  p {
+    font-size: clamp(2rem, 3vw, 2.5rem);
+    font-weight: 600;
+  }
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,98 +1,76 @@
 <template>
-
-
-  <main class="container">
+  <main>
     <Logo />
-    <section>
-      <figure></figure>
-      <p class="word">Woord...</p>
-    </section>
-    <button>
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-        <path d="M0 0h24v24H0z" />
-        <rect x="9" y="2" width="6" height="11" rx="3" />
-        <path d="M5 10a7 7 0 0 0 14 0" />
-        <line x1="8" y1="21" x2="16" y2="21" />
-        <line x1="12" y1="17" x2="12" y2="21" />
-      </svg>
-    </button>
+    <Word :word="word" />
+    <RecordButton :isRecording="isRecording" :startSpeech="startSpeech" />
   </main>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      recognition: null,
+      word: '',
+      words: ['hond', 'kat', 'vogel', 'vis'],
+      counter: 0,
+      isRecording: false
+    }
+  },
+  mounted() {
+    const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition
+    this.recognition = new SpeechRecognition()
 
+    this.recognition.addEventListener('result', this.onResult)
+    this.recognition.addEventListener('speechend', this.onSpeechEnd)
+  },
+  methods: {
+    startSpeech() {
+      this.isRecording = !this.isRecording
+
+      if (this.counter > this.words.length - 1) {
+        this.counter = 0
+      }
+
+      this.word = this.words[this.counter].toLowerCase()
+
+      this.recognition.lang = 'nl-NL'
+      this.recognition.continuous = false
+      this.recognition.interimResults = false
+      this.recognition.maxAlternatives = 1
+      this.recognition.start()
+    },
+    onResult(event) {
+      this.recognition.stop()
+      this.isRecording = false
+      const speechResult = event.results[0][0].transcript.toLowerCase()
+
+      if (speechResult === this.word) {
+        this.counter++
+        document.body.style.background = 'lime'
+      } else {
+        document.body.style.background = '#ffc16f'
+      }
+
+      console.log(speechResult)
+      console.log('Confidence: ' + event.results[0][0].confidence)
+    },
+    onSpeechEnd() {
+      this.recognition.stop()
+      this.isRecording = false
+    }
+  }
 }
 </script>
 
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-evenly;
-  align-items: center;
-  text-align: center;
-}
-
-button {
-  appearance: none;
-  background: white;
-  border: none;
-  border-radius: 50%;
-  box-shadow: 0 0 1em 0.25em #00000015;
-  display: block;
-  font-size: 1.25rem;
-  height: 2.5em;
-  padding: .25em;
-  margin: 0 auto;
-  width: 2.5em;
-}
-
-button svg {
-  --size: 2em;
-  fill: none;
-  height: var(--size);
-  stroke: #8485b4;
-  stroke-linecap: round;
-  stroke-linejoin: round;
-  stroke-width: 1.5;
-  width: var(--size);
-}
-
-button svg path:first-of-type {
-  fill: none;
-  stroke: none;
-}
-
-button div {
-  display: flex;
-  justify-content: space-evenly;
-}
-
-button div i {
-  animation: musicEqualizer 400ms ease var(--delay, 0s) infinite alternate;
-  background-image: linear-gradient(
-    to top,
-    #fbc2eb 0%,
-    #a6c1ee 100%
-  );
-  display: block;
-  height: 1rem;
-  width: .125rem;
-}
-
-button div i:nth-of-type(even) {
-  --delay: 400ms
-}
-
-@keyframes musicEqualizer {
-  from {
-    transform: scaleY(1);
+<style lang="css" scoped>
+  main {
+    margin: 0 auto;
+    min-height: 100vh;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-evenly;
+    align-items: center;
+    text-align: center;
   }
-  to {
-    transform: scaleY(1.5)
-  }
-}
 </style>
