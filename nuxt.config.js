1441f9f57c3c2dd68a567a23ad96189f80c2ecb7
MarvinMichel <mmhsernee@gmail.com>
2021-05-26 10:30:59 +0200

    Add build directory

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,11 +14,11 @@ export default {
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
     ]
   },
-
   server: {
     port: 3000,
     host: '0.0.0.0'
   },
+  buildDir: 'nuxt-dist',
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
