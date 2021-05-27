cb220523e265906705d9fc198e49fa9813543071
Roeland <roelandsteevan@gmail.com>
2021-05-27 15:57:37 +0200

    progress bar with divs

---
+++ components/ProgressBar.vue
@@ -0,0 +1,27 @@
+<template>
+	<div class="progress-background">
+    <div class="progress-bar"></div>
+  </div>
+</template>
+
+<style scoped>
+	.progress-background,
+	.progress-bar {
+		height: 20px;
+		border-radius: 100px;
+	}
+
+	.progress-background {
+		width: 80%;
+		background-color: #e0e0e0;
+	}
+
+  .progress-bar {
+		height: 20px;
+		width: 0;
+		min-width: 20px;
+		margin-right: auto;
+		background-color: #77DB81;
+		transition: 1s all;
+  }
+</style>
