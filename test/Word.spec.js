669f177dbdc1c8667678ef8c6d2fb37672319177
Roeland <roelandsteevan@gmail.com>
2021-06-16 15:41:57 +0200

    removed files

--- test/Word.spec.js
+++
@@ -1,9 +0,0 @@
-import { mount } from '@vue/test-utils'
-import Word from '@/components/Word.vue'
-
-describe('Word', () => {
-  test('is a word', () => {
-    const wrapper = mount(Word)
-    expect(wrapper.vm).toBeTruthy()
-  })
-})
